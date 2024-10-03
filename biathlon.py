d740cb5ff9c60557fc8c2ae745993f975f15bb00
sobl9415 <sofiab374@gmail.com>
2024-10-03 20:03:07 +0200

    Add files via upload

--- biathlon.py
+++ biathlon.py
@@ -1,53 +1,66 @@
 from random import randint
-"I love Ponsimlouma"
+print("WELCOME tooooooo BIATLONHHHH\n")
+print("You have 5 shots\n")
 
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\t      Biathlon\n\n\ta hit or miss game\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print("\nYou got 5 shots")
 
-n = 1
-shots = 5
-tavla = ["*","*","*","*","*"] 
+n=1
+list_numbers=[1,2,3,4,5]
+tavla=["*","*","*","*","*"]
 
-def hit_prob(): #hit probability
-    tf = randint(0,10)
-    if tf > 4:
-        return True
-    else:
-        return False
-    
-def sign():
-    print("\n1 2 3 4 5\n")
+
+def hlp_list(): #funktion för listan som visar numret på varje "bricka" i tavlan
+    for i in list_numbers:
+        print(i, end=" ")
+    print()
+
+def sign(): #funktion som skriver ut tavlan
     for i in tavla:
-         print(i, end=' ')
-         
-def shoot(n):
-    while True:
-        s = int(input(f"\n\nShot nr {n} at target: "))
-        if s < 6 and s > 0:
-            return s
-        else:
-            print("\nTargets 1-5")
+        print(i, end=" ")
+    print()
 
-def game_round(n):
-    while n < 6:
-        sign()
-        s = shoot(n)
-        t = tavla[s-1] 
-        x = hit_prob() # träff eller miss
-        if x:
-            print("\nHit on open target")
-            tavla[s-1] = 0
-            n = n+1
-        else:
-            print("\nMiss")
-            n = n+1
+hlp_list()
+sign() 
+
+def prob(): #funktion för sannorlikheten
+    tf=randint(0,10)
+    if tf>4:
+        return True #träff
+    else:
+        return False #miss
         
+def in_game(n): #funktion som kör spelet 
+    shots=5
+    while shots > 0: #körs så länge skott finns kvar
+        s=int(input(f"Shot nr {n} at target: "))
+        
+        if s <= 0 or s > len(tavla): #om input är utanför tavlans intervall
+            print("Targets 1-5")
+            continue #annars fortsätt
+        
+        if tavla[s-1]==("0"): #Om man skjuter på något redan träffat
+            print("Shot at open target")
+            shots=shots-1
+            n=n+1
+            continue
+        
+        else:
+            
+            if tavla[s-1]==("*") and prob(): #Om man skjuter på en stängd bricka samt sannorlikheten är träff
+                tavla[s-1]=("0") #Ersätt brickan med en open target
+                print("Hit\n")
+                n=n+1
+            else:
+                print("Miss\n")
+                n=n+1
+            
+        shots=shots-1
+        hlp_list()
+        sign()   
+        
+        
+in_game(n)
+            
 
-game_round(n)  
-           
+        
+   
     
-    
-
-
-#def skott(mål):
-#    for i in 
